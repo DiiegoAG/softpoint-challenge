35ce334d6de864f76e80f86926c6b2c012cf6760
Diego Arreola <diegodaag07@gmail.com>
2026-02-23 00:04:49 -0600

    refactor: user info now is saved after logged action

--- frontend/src/modules/auth/hooks/useRegister.ts
+++ frontend/src/modules/auth/hooks/useRegister.ts
@@ -1,14 +1,15 @@
 import { useMutation } from '@tanstack/react-query'
 import { authService } from '../services/authService'
-import { useAuthStore } from '@/store/useAuthStore'
+import { useAuthStore } from '@/stores/useAuthStore'
 
 export const useRegister = () => {
-    const setToken = useAuthStore((state) => state.setToken)
+    const { setToken, setUser } = useAuthStore()
 
     return useMutation({
         mutationFn: authService.register,
         onSuccess: (data) => {
             setToken(data.token)
+            setUser(data.user)
         },
     })
 }
