6e804cb9dcd537adc75724938245a71496f887b0
Diego Arreola <diegodaag07@gmail.com>
2026-02-22 19:28:59 -0600

    feat: DashboardPage is created

---
+++ frontend/src/modules/dashboard/pages/DashboardPage.tsx
@@ -0,0 +1,10 @@
+function DashboardPage() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold mb-4">Bienvenido al Dashboard</h1>
+            <p className="text-lg text-gray-600">Aquí puedes ver tus datos y estadísticas.</p>
+        </div>
+    );
+}
+
+export default DashboardPage;
