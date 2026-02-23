6cdbef7dc51f922f19f9513e46193110ab7447d3
Diego Arreola <diegodaag07@gmail.com>
2026-02-23 00:05:07 -0600

    feat: register page route is added

--- frontend/src/modules/auth/router.tsx
+++ frontend/src/modules/auth/router.tsx
@@ -1,11 +1,16 @@
 import { lazy } from "react";
 
 const LoginPage = lazy(() => import("./pages/LoginPage"));
+const RegisterPage = lazy(() => import("./pages/RegisterPage"));
 
 const AuthRoutes = [
     {
         path: "/login",
         element: <LoginPage />,
+    },
+    {
+        path: "/register",
+        element: <RegisterPage />,
     },
 ]
 
