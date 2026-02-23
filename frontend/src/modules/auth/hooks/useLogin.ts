f3d061e60dc37b7fbd9b7075cd881fd6d89b1ac4
Diego Arreola <diegodaag07@gmail.com>
2026-02-22 20:29:15 -0600

    refactor: user's information is saved now in zustand's user store

--- frontend/src/modules/auth/hooks/useLogin.ts
+++ frontend/src/modules/auth/hooks/useLogin.ts
@@ -3,12 +3,13 @@ import { authService } from '../services/authService'
 import { useAuthStore } from '@/stores/useAuthStore'
 
 export const useLogin = () => {
-    const setToken = useAuthStore((state) => state.setToken)
+    const { setToken, setUser } = useAuthStore()
 
     return useMutation({
         mutationFn: authService.login,
         onSuccess: (data) => {
             setToken(data.token)
+            setUser(data.user)
         },
     })
 }
